40df17e0c852161839c28e59bdc4fc40cabbe3b6
Pratya S <pratya.kee@hotmail.com>
2021-08-05 10:21:04 +0700

    Commit Lab05

--- Lab05_Cylindrical/Program.cs
+++ Lab05_Cylindrical/Program.cs
@@ -6,7 +6,24 @@ namespace Lab05_Cylindrical
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); //aa
+            // 1 : Declare Variable 
+            int radius = 0;
+            int height = 0;
+            double area = 0;
+             
+
+            // 2 : Input 
+            Console.Write("Enter Radius : ");
+            radius = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Height : ");
+            height = Convert.ToInt32(Console.ReadLine());
+
+            // 3 : Process 
+            area = Math.PI * radius * radius * height;
+
+            //4 : Output 
+            Console.WriteLine("Area is : {0}", area);
         }
     }
 }
