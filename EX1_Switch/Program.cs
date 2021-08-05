a1940c7f42e0a21adab98c8461b900acb2cd3fe3
Pratya S <pratya.kee@hotmail.com>
2021-08-05 12:08:37 +0700

    ADD EX1 : Switch Case

---
+++ EX1_Switch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace EX1_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu = 0;
+
+            menu = Convert.ToInt32(Console.ReadLine());
+
+            switch (menu)
+            {
+                case 101:
+                    Console.WriteLine("Deposite");
+                    break;
+
+                case 201:
+                    Console.WriteLine("Withdraw");
+                    break;
+
+                case 301:
+                    Console.WriteLine("Check Balance");
+                    break;
+
+                default:
+                    Console.WriteLine("Please Enter Menu Code");
+                    break;
+            }
+        }
+    }
+}
