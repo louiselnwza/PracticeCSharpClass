71cf50fa1db306bc4628465358af9a76af4c0aaa
Pratya S <pratya.kee@hotmail.com>
2021-08-05 17:06:08 +0700

    Lab 17

---
+++ Lab16_IF_ELSE_14/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lab16_IF_ELSE_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 : Declare Variable 
+            const int ratePerHour = 350;
+            int totalWorkHour = 0;
+            int calSalary = 0;
+            int payTheMoney = 0;
+            int change = 0;
+
+            // 2 : Input 
+            Console.Write("Enter Hour : ");
+            totalWorkHour = Convert.ToInt32(Console.ReadLine());
+            calSalary = totalWorkHour * ratePerHour;
+
+            Console.WriteLine("Total Salary : {0}", calSalary);
+            Console.Write("Pay : ");
+            payTheMoney = Convert.ToInt32(Console.ReadLine());
+
+            // 3 : Process 
+            if (payTheMoney > calSalary)
+            {
+                change = payTheMoney - calSalary;
+                //4 : Output 
+
+                Console.WriteLine("Change : {0}", change);
+            }
+            else
+            {
+                change = calSalary - payTheMoney;
+                //4 : Output 
+ 
+                Console.WriteLine("Please found some money \"{0}\" THB for pay", change);
+            }
+
+
+
+        }
+    }
+}
---
+++ Lab17_IF_ELSE_23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab17_IF_ELSE_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 : Declare Variable 
+            int number = 0;
+            bool isOdd = false;
+
+            // 2 : Input 
+            Console.Write("Enter Number : ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            // 3 : Process 
+            if (number % 2 != 0)
+            {
+                isOdd = true;
+            }
+
+            //4 : Output 
+            Console.WriteLine("Is Odd ? {0}", isOdd);
+        }
+    }
+}
