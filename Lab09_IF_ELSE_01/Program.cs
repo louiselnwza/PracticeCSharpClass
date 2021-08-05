180d6fa3d68328bb8aef0ce38e0a1993b1c1d9f3
Pratya S <pratya.kee@hotmail.com>
2021-08-05 13:46:55 +0700

    Lab09

---
+++ Lab09_IF_ELSE_01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab09_IF_ELSE_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 : Declare Variable 
+            int number = 0;
+            bool isMoreThan10 = false;
+
+            // 2 : Input 
+            number = Convert.ToInt32(Console.ReadLine());
+
+            // 3 : Process 
+            if (number > 10)
+            {
+                Console.WriteLine("More than 10");
+                //isMoreThan10 = true;
+            }
+
+            //4 : Output 
+            //Console.WriteLine("Is number more than 10 ? {0}", isMoreThan10);
+
+        }
+    }
+}
--- Lab4_TriangleArea/Program.cs
+++ Lab4_TriangleArea/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Lab4_TriangleArea
+namespace Lab04_TriangleArea
 {
     class Program
     {
