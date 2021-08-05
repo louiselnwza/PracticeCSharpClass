246d43a0b24a6c557821697ef14c650332c9a0e5
Pratya S <pratya.kee@hotmail.com>
2021-08-05 14:47:53 +0700

    Lab11

---
+++ Lab11_IF_ELSE_07/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab11_IF_ELSE_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 : Declare Variable 
+            string text = "";
+
+            // 2 : Input 
+            Console.Write("Enter Text : ");
+            text = Console.ReadLine();
+
+            // 3 : Process 
+            if (text.ToUpper() == "A")
+            {
+                //4 : Output 
+                Console.WriteLine("IS A");
+            }
+            else
+            {
+                //4 : Output 
+                Console.WriteLine("IS NOT A");
+            }
+        }
+    }
+}
