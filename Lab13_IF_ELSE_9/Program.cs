d3b3c48d7f555ede9e2e48f49ad141a86b1ed4d9
Pratya S <pratya.kee@hotmail.com>
2021-08-05 15:01:26 +0700

    Lab13

---
+++ Lab13_IF_ELSE_9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lab13_IF_ELSE_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 : Declare Variable 
+            string text = "";
+            int number = 0;
+
+            // 2 : Input 
+            Console.Write("Enter Text : ");
+            text = Console.ReadLine();
+
+            Console.Write("Enter Number : ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            // 3 : Process 
+            if (text.ToUpper() != "Y")
+            {
+                // True
+                //4 : Output 
+                Console.WriteLine("{0} IS NOT Y", number);
+            }
+            else
+            {
+                //False
+                //4 : Output 
+                Console.WriteLine("{0} IS Y", number);
+            }
+        }
+    }
+}
