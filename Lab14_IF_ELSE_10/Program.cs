f8102d10701fa9a8c0989e6b3ad6842f3c38f541
Pratya S <pratya.kee@hotmail.com>
2021-08-05 16:07:49 +0700

    Lab15 No12

---
+++ Lab14_IF_ELSE_10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab14_IF_ELSE_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 : Declare Variable 
+            int number1 = 0, number2 = 0;
+
+            // 2 : Input 
+            Console.Write("Enter 1st Number : ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter 2nd Number : ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            // 3 : Process 
+            if (number1 > number2)
+            {
+                Console.WriteLine("{0} > {1}", number1, number2);
+            }
+            else
+            {
+                Console.WriteLine("{0} <= {1}", number1, number2);
+            }
+        }
+    }
+}
---
+++ Lab15_IF_ELSE_12/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Lab15_IF_ELSE_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 : Declare Variable 
+            int money = 0;
+            int THB1000 = 0;
+            int THB500 = 0;
+            int THB100 = 0;
+
+
+            // 2 : Input 
+            Console.Write("Enter Withdraw : ");
+            money = Convert.ToInt32(Console.ReadLine());
+
+            // Assume 3600 
+
+            // 3 : Process 
+            if (money >= 1000)
+            {
+
+                if (money % 1000 == 0)
+                {
+                    THB1000 = money / 1000;
+                }
+                else
+                {
+                    THB1000 = money / 1000; // 3
+                }
+                money = money % 1000;
+            }
+
+            if (money >= 500)
+            {
+
+                if (money % 500 == 0)
+                {
+                    THB500 = money / 500;
+                }
+                else
+                {
+                    THB500 = money / 500; // 3
+                }
+                money = money % 500;
+            }
+
+            if (money >= 100)
+            {
+
+                if (money % 100 == 0)
+                {
+                    THB100 = money / 100;
+                }
+                else
+                {
+                    THB100 = money / 100; // 3
+                }
+                money = money % 100;
+            }
+
+            Console.WriteLine("THB 1000 : {0} || THB 500 : {1} || THB 100 : {2}", THB1000, THB500 ,THB100);
+
+            //if (money >= 500 && money < 1000)
+            //{
+
+            //}
+
+            //if (money >= 100 && money < 500)
+            //{
+
+            //}
+
+
+            //4 : Output 
+        }
+    }
+}
