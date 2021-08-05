dc09c539b1b73e30ac60875ffb20dc8b4b3d1b1f
Pratya S <pratya.kee@hotmail.com>
2021-08-05 14:17:50 +0700

    Lab10 No.6

---
+++ Lab10_IF_ELSE_05/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lab10_IF_ELSE_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 : Declare Variable 
+            int number = 0;
+
+            // 2 : Input 
+            number = Convert.ToInt32(Console.ReadLine());
+            bool isMoreThan10 = false;
+
+
+            #region :: Option 1 :: 
+            // 3 : Process 
+            if (number >= 10)
+            {
+                Console.WriteLine("Equal or More than 10");
+            }
+            else
+            {
+                Console.WriteLine("less than 10");
+            }
+
+            #endregion
+
+            #region :: Option 2 :: 
+            // 3 : Process 
+            if (number >= 10)
+            {
+                isMoreThan10 = true;
+            }
+
+            // 4 : Output 
+            Console.WriteLine("Is this number more than 10 ? {0}", isMoreThan10);
+
+            #endregion
+
+        }
+    }
+}
