f70b495f4deb7d926e9d1eef9392df778320d003
Pratya S <pratya.kee@hotmail.com>
2021-08-05 11:23:37 +0700

    Lab 08

---
+++ Lab08_Mean/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab08_Mean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 : Declare Variable 
+            double mean = 0.0, x1 = 0, x2 = 0, x3 = 0, x4 = 0;
+
+            // 2 : Input 
+            Console.Write("x1 : ");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("x2 : ");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("x3 : ");
+            x3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("x4 : ");
+            x4 = Convert.ToDouble(Console.ReadLine());
+
+            // 3 : Process 
+            mean = (x1 + x2 + x3 + x4) / 4;
+
+            //4 : Output
+            Console.WriteLine("Mean is : {0}", mean);
+        }
+    }
+}
