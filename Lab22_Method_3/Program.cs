4142bc948b0d9b67ab21cfe1b8d3064ad4022e1d
Pratya S <pratya.kee@hotmail.com>
2021-08-06 16:31:12 +0700

    Lab23

---
+++ Lab22_Method_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab22_Method_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 1 : Declare Variable 
+            double width = 0, height = 0, areaOfTriangle = 0;
+
+            // 2 : Input 
+            Console.Write("Enter width : ");
+            width = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter height : ");
+            height = Convert.ToDouble(Console.ReadLine());
+
+            // 3 : Process 
+            areaOfTriangle = CalculateTriangle(width, height);
+            // 4 : Output
+            Console.WriteLine("Area Triangle is {0}", areaOfTriangle);
+        }
+
+        public static double CalculateTriangle(double width, double height)
+        {
+            double areaTrignable = (0.5) * width * height;
+            return areaTrignable;
+        }
+    }
+}
---
+++ Lab23_Loop_5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab23_Loop_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberIsFactorial = 9999;
+            int summary = 1;
+            int i = 1;
+
+
+            Console.Write("Enter your number : ");
+            numberIsFactorial = Convert.ToInt32(Console.ReadLine());
+
+            while (i <= numberIsFactorial)
+            {
+                summary = summary * i;
+                i++;
+            }
+
+            Console.Write("{0}", summary);
+        }
+    }
+}
