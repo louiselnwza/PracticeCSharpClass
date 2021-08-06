d3c5c44d3bc2449926ec3cdd23b5c5042285800f
Pratya S <pratya.kee@hotmail.com>
2021-08-06 11:48:10 +0700

    Lab 21

---
+++ Lab21_Loop_4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lab21_Loop_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0;  i >= -20; i--)
+            {
+                Console.WriteLine("{0}", i);
+            }
+        }
+    }
+}
