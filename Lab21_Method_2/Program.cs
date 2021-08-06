5d1a3f8f028ce6572a23efd200cd220034a32c0d
Pratya S <pratya.kee@hotmail.com>
2021-08-06 15:41:17 +0700

    Lab 21

---
+++ Lab21_Method_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab21_Method_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+
+            Console.Write("Please Enter the number : ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            PrintSign(number);
+
+        }
+
+        public static void PrintSign(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine("I +");
+            }
+            else if (number == 0 )
+            {
+                Console.WriteLine("I0");
+            }
+            else
+            {
+                Console.WriteLine("I-");
+            }
+        }
+    }
+}
