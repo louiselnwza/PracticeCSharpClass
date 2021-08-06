741929213a592d5c9fc53940e35955b17317e13f
Pratya S <pratya.kee@hotmail.com>
2021-08-06 11:04:31 +0700

    Commit Lab 19

---
+++ Lab19_Loop_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab19_Loop_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 : Declare Variable 
+            double averageScoreOfStudent = 0;
+            double summaryScoreOfStudent = 0;
+            int iCountNoOfStudent = 0;
+            double scoreOfStudent = 0;
+            bool isPositive = true;
+
+            // 2 : Input 
+            while (isPositive)
+            {
+                Console.Write("Enter a score : ");
+                scoreOfStudent = Convert.ToDouble(Console.ReadLine());
+
+                if (scoreOfStudent >= 0)
+                {
+                    summaryScoreOfStudent = summaryScoreOfStudent + scoreOfStudent;
+                    iCountNoOfStudent = iCountNoOfStudent + 1; // iCountNoOfStudent++
+                }
+                else
+                {
+                    isPositive = false;
+                }
+            }
+            averageScoreOfStudent = (summaryScoreOfStudent / iCountNoOfStudent);
+
+            Console.WriteLine("Number of Student is {0}", iCountNoOfStudent);
+            Console.WriteLine("Average score is {0} ", string.Format("{0:N2}", averageScoreOfStudent));
+
+        }
+    }
+}
